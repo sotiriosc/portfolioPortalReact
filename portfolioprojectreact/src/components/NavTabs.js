9f3c4ac1c48ce2bc6080696c2d8a2d5bd3bac6e8
sotiriosc <chortogiannoss@gmail.com>
2023-05-12 18:17:51 -0400

    added img fixing header

--- portfolioprojectreact/src/components/NavTabs.js
+++ portfolioprojectreact/src/components/NavTabs.js
@@ -1,52 +1,48 @@
 import React from 'react';
+import selfimg from '../assets/images/selfimg.jpg';
+import '../styles/NavTab.css';
 
 // TODO: Add a comment explaining how we are able to extract the key value pairs from props
 // We are able to extract the key value pairs from props by using the curly braces and the key name.
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
+    <header class='header'>
+      <div>
+        <h1>Sotirios Chortogiannos</h1>
+
+       
+      
+    <ul className="navTabs">
+      <li className="pageBtn">
         <a
           href="#home"
           onClick={() => handlePageChange('Home')}
-          //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
-          //*  This is a ternary operator that is checking to see if the 
-          //currentPage is equal to 'Home'. If it is, then it will return 
-          //the 'nav-link active' class. If it is not, then it will return 
-          //the 'nav-link' class.
           className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
         >
           Home
         </a>
       </li>
-      <li className="nav-item">
+      <li className="pageBtn">
         <a
           href="#about"
           onClick={() => handlePageChange('About')}
-          //  TODO: Add a comment explaining what this logic is doing
-          //This logic is checking to see if the currentPage is equal to 'About'.
-
           className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
         >
           About
         </a>
       </li>
-      <li className="nav-item">
+      <li className="pageBtn">
         <a
           href="#myWork"
           onClick={() => handlePageChange('MyWork')}
-          //  TODO: Add a comment explaining what this logic is doing
-          //This logic is checking to see if the currentPage is equal to 'Blog'.
           className={currentPage === 'MyWork' ? 'nav-link active' : 'nav-link'}
         >
           My Work
         </a>
       </li>
-      <li className="nav-item">
+      <li className="pageBtn">
         <a
           href="#contact"
-          //  TODO: Add a comment explaining what this logic is doing
-          //This logic is checking to see if the currentPage is equal to 'Contact'.
           onClick={() => handlePageChange('Contact')}
           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
         >
@@ -54,7 +50,11 @@ function NavTabs({ currentPage, handlePageChange }) {
         </a>
       </li>
     </ul>
+    </div>
+    <img src={selfimg} alt="Sotirios Chortogiannos" class="image" />
+    </header>
   );
 }
 
 export default NavTabs;
+
