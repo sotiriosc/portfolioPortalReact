6c33b0de0dc49a49827ca54555b3044106b87560
sotiriosc <chortogiannoss@gmail.com>
2023-05-13 21:59:08 -0400

    Updates

---
+++ precache-manifest.26d83271203e0567121f83b7731646ce.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "37ac2a8043e0caac1ae5",
+    "url": "/sotiriosc/portfolioPortalReact/static/css/main.a94055db.chunk.css"
+  },
+  {
+    "revision": "37ac2a8043e0caac1ae5",
+    "url": "/sotiriosc/portfolioPortalReact/static/js/main.37ac2a80.chunk.js"
+  },
+  {
+    "revision": "c8218068c20377719002",
+    "url": "/sotiriosc/portfolioPortalReact/static/css/1.4ef52dde.chunk.css"
+  },
+  {
+    "revision": "c8218068c20377719002",
+    "url": "/sotiriosc/portfolioPortalReact/static/js/1.c8218068.chunk.js"
+  },
+  {
+    "revision": "25d81103eacbf2156a32",
+    "url": "/sotiriosc/portfolioPortalReact/static/js/runtime~main.25d81103.js"
+  },
+  {
+    "revision": "0c5ca68c1013df8371e265bcfe1c2f5a",
+    "url": "/sotiriosc/portfolioPortalReact/static/media/selfimg.0c5ca68c.jpg"
+  },
+  {
+    "revision": "4a707f98e9471c1d08b028cd2e29123f",
+    "url": "/sotiriosc/portfolioPortalReact/static/media/peach.4a707f98.jpg"
+  },
+  {
+    "revision": "c4dbaa0e9cd833502122ef08996f8637",
+    "url": "/sotiriosc/portfolioPortalReact/static/media/mango.c4dbaa0e.jpg"
+  },
+  {
+    "revision": "8f2912a6f8fae54e99313897e8deb308",
+    "url": "/sotiriosc/portfolioPortalReact/static/media/grape.8f2912a6.jpg"
+  },
+  {
+    "revision": "f2629bb743fd9744638d2b9ede52caf3",
+    "url": "/sotiriosc/portfolioPortalReact/static/media/orders.f2629bb7.jpg"
+  },
+  {
+    "revision": "ea48883685a9427b74db9f3afa2f043b",
+    "url": "/sotiriosc/portfolioPortalReact/static/media/sumo.ea488836.jpg"
+  },
+  {
+    "revision": "5f6d262014741fd027edde8bdfd15642",
+    "url": "/sotiriosc/portfolioPortalReact/static/media/tomato.5f6d2620.jpg"
+  },
+  {
+    "revision": "516170221c6db44491a6cd707c0083f0",
+    "url": "/sotiriosc/portfolioPortalReact/static/media/Myresume.51617022.docx"
+  },
+  {
+    "revision": "5b2ab9791c11d1ed9d9308efee91db6c",
+    "url": "/sotiriosc/portfolioPortalReact/static/media/project1.5b2ab979.png"
+  },
+  {
+    "revision": "e5888459ed9375f40ca6c434a5527a6d",
+    "url": "/sotiriosc/portfolioPortalReact/static/media/project2.e5888459.png"
+  },
+  {
+    "revision": "1fafedab409419ce54ff8c67ea8bbf97",
+    "url": "/sotiriosc/portfolioPortalReact/static/media/revved.1fafedab.png"
+  },
+  {
+    "revision": "32a778fc31102283e31502ff60004460",
+    "url": "/sotiriosc/portfolioPortalReact/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/sotiriosc/portfolioPortalReact/precache-manifest.26d83271203e0567121f83b7731646ce.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/sotiriosc/portfolioPortalReact/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
