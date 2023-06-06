6fe482ac414067d2e05d3f41af99be2933ac2d3d
sotiriosc <chortogiannoss@gmail.com>
2023-06-06 08:38:14 -0400

    updated resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../styles/Resume.css';
-import resume from '../../assets/files/Myresume.docx';
+import resume from '../../assets/files/Myresumeroughdraft.docx';
 
 export default function About() {
   return (
