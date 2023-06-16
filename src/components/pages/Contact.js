95cd204f9b3622afe4b15d5805e2f9ffc85412c8
sotiriosc <chortogiannoss@gmail.com>
2023-06-15 20:57:16 -0400

    added resume fixed form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,115 +1,29 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../styles/Contact.css';
-import { validateEmail } from '../../utils/helpers';
 
-// This function sets up the Contact page initial settings and state
 export default function Contact() {
-  const [form, setForm] = useState({ 
-    name: '', email: '', message: '', 
-    nameValid: false, emailValid: false, messageValid: false, 
-    nameTouched: false, emailTouched: false, messageTouched: false,
-  });
-  const [modalOpen, setModalOpen] = useState(false);
-  const [modalMessage, setModalMessage] = useState('');
+  const [height, setHeight] = useState(window.innerHeight);
 
-  const modalShown = useRef({name: false, email: false, message: false});
+  // Add an event listener to the window to adjust the height state 
+  // whenever the window size changes
+  useEffect(() => {
+    const handleResize = () => setHeight(window.innerHeight);
+    window.addEventListener('resize', handleResize);
+    
+    // Clean up the event listener when the component unmounts
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-  // This function handles the input change event
-  const handleInputChange = (e) => {
-    setForm(prevForm => ({ ...prevForm, 
-      [e.target.name]: e.target.value,
-      [`${e.target.name}Touched`]: true 
-    }));
-  };
-  
-  // This function handles the input blur event
-  const handleInputBlur = (e) => {
-    let isValid;
-    if (e.target.name === 'email') {
-      isValid = validateEmail(e.target.value);
-    } else {
-      // For 'name' and 'message' fields, checking if at least one character is present
-      isValid = e.target.value.trim().length > 0;
-    }
-  
-    if (!isValid && !modalShown.current[e.target.name]) {
-      const errorMessage = e.target.name === 'email' ? 'Input must be a valid email address' : 'This field requires 1 or more characters';
-      openModal(errorMessage);
-      modalShown.current[e.target.name] = true;
-    }
-    // This function handles the input change event
-    setForm(prevForm => ({ ...prevForm, 
-      [`${e.target.name}Valid`]: isValid
-    }));
-  };
-  
-  // This function handles the form submission event
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add form submission logic here
-  };
-
-  // This function opens the modal with an error message
-  const openModal = (message) => {
-    console.log('openModal called'); 
-    setModalMessage(message);
-    setModalOpen(true);
-  };
-  
-  // This function closes the modal
-  const closeModal = () => {
-    setModalOpen(false);
-  };
-
-  // This Function creates the page layout and content
   return (
     <div className='contact'>
-      <form className='contactForm' onSubmit={handleSubmit}>
-        <h2>Contact Page:</h2>
-        <label>
-          Name:
-          <input
-            type='text'
-            name='name'
-            value={form.name}
-            onChange={handleInputChange}
-            onBlur={handleInputBlur}
-            className={form.nameTouched ? (form.nameValid ? 'valid' : 'invalid') : ''}
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            type='email'
-            name='email'
-            value={form.email}
-            onChange={handleInputChange}
-            onBlur={handleInputBlur}
-            className={form.emailTouched ? (form.emailValid ? 'valid' : 'invalid') : ''}
-          />
-        </label>
-        <label className='msgLabel'>
-          Message:
-          <textarea
-            name='message'
-            className={`textArea ${form.messageTouched ? (form.messageValid ? 'valid' : 'invalid') : ''}`}
-            value={form.message}
-            onChange={handleInputChange}
-            onBlur={handleInputBlur}
-          />
-        </label>
-
-        <input type='submit' value='Submit' />
-      </form>
-
-      {modalOpen && (
-        <div className='modal'>
-          <div className='modalContent'>
-            <p>{modalMessage}</p>
-            <button onClick={closeModal} className='modalBtn'>Close</button>
-          </div>
-        </div>
-      )}
+      <iframe 
+        src="https://docs.google.com/forms/d/e/1FAIpQLSeg5wh1PDr0DnPFmPd1e36BzgztCDDlMziEtIK8VHSYFF1LGw/viewform?usp=pp_url"
+        style={{ width: '100%', height: `${height}px`, border: '0', overflow: 'hidden' }}
+        scrolling="yes"
+        title="Contact Form"
+      >
+        Loading…
+      </iframe>
     </div>
   );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../styles/Resume.css';
-import resume from '../../assets/files/Myresumeroughdraft.pdf';
+import resume from '../../assets/files/Resume.pdf';
 
 export default function About() {
   return (
